9a20f70af6e1149dc6e49e89b02b2d65cf845138
nilsreich <nilsreich@gmail.com>
2023-09-08 07:08:10 +0000

    init

---
+++ app/page.tsx
@@ -0,0 +1,38 @@
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
+import Link from 'next/link'
+import LogoutButton from '../components/LogoutButton'
+
+
+export default async function Index() {
+  const supabase = createServerComponentClient({ cookies })
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser()
+
+  return (
+    <div className="w-full flex flex-col items-center">
+      <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+
+          <div>
+            {user ? (
+              <div className="flex items-center gap-4">
+                Hey, {user.email}!
+                <LogoutButton />
+              </div>
+            ) : (
+              <Link
+                href="/login"
+                className="py-2 px-4 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover"
+              >
+                Login
+              </Link>
+            )}
+          </div>
+      </nav>
+
+<div>asd</div>
+    </div>
+  )
+}
